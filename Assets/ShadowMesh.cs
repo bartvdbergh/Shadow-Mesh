0e4186e409bae80774ea4f41d2e825c697aa2d2d
Bartvdb <bartvdb@outlook.be>
2016-06-08 14:23:48 +0200

    Projection morphs mesh to shadow
    Modified the script to morph the projection mesh to match the shape of
    the shadow. Dynamicly adds/removes vertices if needed.
    
    No new normals because I personally only need the bounding box. Not
    optimized

--- Assets/ShadowMesh.cs
+++ Assets/ShadowMesh.cs
@@ -1,75 +1,210 @@
 ﻿using UnityEngine;
 using System.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ShadowMesh : MonoBehaviour
 {
-    private void ProjectVertcies(Light source, GameObject obj, out Vector3[] verticies, out Vector3[] normals)
-    {
-        verticies = obj.GetComponent<MeshFilter>().mesh.vertices;
-        normals = new Vector3[verticies.Length];
+	public Light source;
+	public GameObject cube;
+	public float normalScale;
 
-        Vector3 sourcePos = source.transform.position;
-        Vector3[] projectedVerticies = new Vector3[verticies.Length];
+	private void Update ()
+	{
+		List<Vector3> vertices;
+		Vector3[] normals;
+		List<int>triangles = new List<int> ();
+		List<int>indices;
 
-        for (var i=0;i<verticies.Length;i++)
-        {
-            Ray direction = new Ray(sourcePos, obj.transform.TransformPoint(verticies[i]) - sourcePos);
-            RaycastHit[] hits = Physics.RaycastAll(direction);
+		ProjectVertcies(source, cube, out vertices, out normals);
+			//-- Remove duplicates, and a 0,0,0 point --//
+		vertices = TrimList (vertices);
+			//-- Order vertices on y-axis so we can assign indeces in clockwise order --//
+		vertices = vertices.OrderBy(v=>v.y).ToList();
+		indices = SetIndices (vertices);
+			//-- Sets the order of the verts so that vertices[indices[0]] = vertices[0], this means when you draw a line from vertix to vertix, you get an outline. Necessary for triangulation --//
+		SortVertsToIndices (ref vertices, ref indices);
+			//-- Copy and translate the existing vertices and indices to create depth --//
+		vertices = ExtrudeVerts (vertices,Vector3.back);
+		indices = ExtrudeIndices (indices);
+			
+			//-- Triangulate the shape --//
+		triangles = ExtrudeTriangles (indices);
+			//-- Feed the information back in to  the mesh --//
+		CreateMesh(vertices, triangles);
+		//DrawDebug (vertices,indices,triangles);
+		//DrawOutline (vertices, indices);
 
-            if (hits.Length > 0)
-            {
-                if (hits[0].collider.gameObject != obj)
-                {
-                    Transform transform = hits[0].transform;
-                    normals[i] = hits[0].normal;
+	}
 
-                    projectedVerticies[i] = (hits[0].point);
+	private void ProjectVertcies (Light source, GameObject obj, out List<Vector3> vertices, out Vector3[] normals)
+	{
+		vertices = obj.GetComponent<MeshFilter> ().mesh.vertices.ToList ();
+		normals = new Vector3[vertices.Count];
 
-                    // DEBUG
-                    Debug.DrawLine(source.transform.position, hits[0].point, Color.red);
-                    Debug.DrawRay(hits[0].point, hits[0].normal, Color.yellow);
-                }
-                else if (hits.Length > 1)
-                {
-                    Transform transform = hits[1].transform;
-                    normals[i] = hits[1].normal;
+		Vector3 sourcePos = source.transform.position;
+		List<Vector3> projectedVerticies = new List<Vector3> ();
 
-                    projectedVerticies[i] = (hits[1].point);
+			//-- Project the outside vertices of the object to the wall --//
+		for (var i = 0; i < vertices.Count; i++) {
+			Ray direction = new Ray (sourcePos, obj.transform.TransformPoint (vertices [i]) - sourcePos);
+			RaycastHit[] hits = Physics.RaycastAll (direction);
 
-                    // DEBUG
-                    Debug.DrawLine(source.transform.position, hits[1].point, Color.red);
-                    Debug.DrawRay(hits[1].point, hits[1].normal, Color.yellow);
-                }
-            }
-        }
-        
-        verticies = projectedVerticies;
-    }
+			if (hits.Length > 0) {
+				if (hits [0].collider.gameObject != obj) {
+					Transform transform = hits [0].transform;
+					normals [i] = hits [0].normal;
+					projectedVerticies.Add(hits [0].point);
 
-    private void ManuiplateCuboid(Vector3[] verticies)
-    {
-        Vector3 averagePosition = new Vector3(verticies.Average(vector => vector.x),
-                                              verticies.Average(vector => vector.y),
-                                              verticies.Average(vector => vector.z));
-        Vector3 averageScale = new Vector3(verticies.Max(vector => vector.x) - verticies.Min(vector => vector.x),
-                                           verticies.Max(vector => vector.y) - verticies.Min(vector => vector.y),
-                                           normalScale);
+					// DEBUG
+					//Debug.DrawLine(source.transform.position, hits[0].point, Color.red);
+					//Debug.DrawRay(hits[0].point, hits[0].normal, Color.yellow);
+				}
+			}
+		}
+		vertices = projectedVerticies;
+	}
 
-        transform.position = averagePosition;
-        transform.localScale = averageScale;
-    }
+	private List<Vector3> TrimList (List<Vector3> list)
+	{
+		List<Vector3>  trim = new List<Vector3>();
+		trim = list.Distinct ().ToList ();
+		trim.Remove (Vector3.zero);
+		return trim;
+	}
 
-    public Light source;
-    public GameObject cube;
-    public float normalScale;
+	private void DrawOutline (List<Vector3> vertices, List<int>indices)
+	{
+		for (int i = 0; i < vertices.Count; i++) {
+			Debug.DrawLine (vertices [i], vertices [(i + 1) % vertices.Count], Color.blue);
+		}
+	}
 
-    private void LateUpdate()
-    {
-        Vector3[] verticies;
-        Vector3[] normals;
+	private List<int>SetIndices (List<Vector3>  vertices)
+	{
+		List<int>indices = new List<int> ();
+		indices.Add (0);
+		int[] selectedIds = new int[vertices.Count];
+		selectedIds [0] = 1;
 
-        ProjectVertcies(source, cube, out verticies, out normals);
+			//-- Set indices on the right side of the top vertex from top to bottom --//
+		for (int i = 1; i < vertices.Count - 1; ++i) {
+			if (isLeft(vertices[vertices.Count - 1],vertices[0],vertices[i])){
+				indices.Add (i);
+				selectedIds [i] = 1;
+				Debug.DrawRay (vertices [i], Vector3.back, new Color(100 + 75 * i,0,0));
+			}
+		}
 
-        ManuiplateCuboid(verticies);
-    }
+			//-- Set indices on the left side of the bottom vertex from bottom to top--//
+		for (int i = vertices.Count - 1; i > 0; i--) {
+			if (selectedIds [i] == 0) {
+				indices.Add (i);
+				//Debug.DrawRay (vertices [i], Vector3.back, Color.green);
+			}
+		}
+		indices.Reverse ();
+		return indices;
+	}
+
+	private bool isLeft(Vector3 a, Vector3 b, Vector3 c){
+		return ((b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x)) >= 0;
+	}
+
+	private void DrawDebug(List<Vector3> vertices, List<int>indices, List<int>triangles){
+
+			//-- Draw the top vertix in white, the bottom in black --//
+		Debug.DrawRay (vertices [0], Vector3.back, Color.white);
+		Debug.DrawRay (vertices [vertices.Count - 1], Vector3.back, Color.black);
+		Debug.DrawRay (vertices [0], Vector3.left, Color.yellow);
+		Debug.DrawRay (vertices [0], Vector3.right, Color.yellow);
+
+			//-- Draw wireframe --//
+		/*for (int i = 0; i < vertices.Count; ++i) {
+			Debug.DrawLine (vertices[triangles[i*3]],vertices[triangles[(i*3)+1]],Color.green);
+			Debug.DrawLine (vertices[triangles[(i*3)+1]],vertices[triangles[(i*3)+2]],Color.green);
+			Debug.DrawLine (vertices[triangles[(i*3)+2]],vertices[triangles[(i*3)]],Color.green);
+		}*/
+	}
+
+	private void CreateMesh(List<Vector3> vertices,List<int>triangles){
+		Mesh mesh = GetComponent <MeshFilter>().mesh;
+		mesh.Clear ();
+		mesh.vertices = vertices.ToArray ();
+		mesh.triangles = triangles.ToArray ();
+
+		
+
+		mesh.RecalculateNormals ();
+		GetComponent <MeshCollider> ().sharedMesh.Optimize();
+		mesh.RecalculateBounds ();
+		GetComponent <MeshCollider> ().sharedMesh = mesh;
+	}
+
+	private List<Vector3>  ExtrudeVerts(List<Vector3>  vertices, Vector3 offset)
+	{
+		int count = vertices.Count;
+		for (int i = 0; i < count; ++i) {
+			vertices.Add(vertices[i] + offset);
+		}
+		return vertices;
+	}
+
+	private List<int> ExtrudeIndices(List<int> indices){
+		int count = indices.Count;
+		for (int i = 0; i < count; i++) {
+			indices.Add(indices[i] + count);
+		}
+
+		return indices;
+	}
+
+	private List<int> ExtrudeTriangles(List<int> indices){
+		int count = indices.Count / 2;
+		List <int> tris = new List<int> ();
+
+		//triangulate the 2 parrallel planes
+		for (int k = 0; k < 2; k++) {
+			for (int i = 1; i < count-1; i++) {
+				tris.Add(indices [0 + (count * k)]);
+				tris.Add(indices [i + (count* k)]);
+				tris.Add(indices [i + 1 + (count * k)]);
+			}
+		}
+
+		//Triangulate te border
+		for (int i = 0; i < count-1; i++) {
+			tris.Add (indices [count+i+1]);
+			tris.Add (indices [count + i]);
+			tris.Add (indices [i]);
+
+
+			tris.Add (indices [i+1]);
+			tris.Add (indices [count+i+1]);
+			tris.Add (indices [i]);
+		}
+
+		int j = count-1;
+		tris.Add (count);
+		tris.Add ((2*count) -1);
+		tris.Add (j);
+
+		tris.Add (0);
+		tris.Add (count);
+		tris.Add (j);
+
+		return tris;
+	}
+
+	private void SortVertsToIndices(ref List<Vector3> vertices, ref List<int>indices){
+		List<Vector3> verts = new List<Vector3>();
+		for (int i = 0; i < indices.Count; i++) {
+			verts.Add(vertices[indices[i]]);
+		}
+
+		vertices = verts;
+		indices = indices.OrderBy(v=>v).ToList();
+	}
+
 }
